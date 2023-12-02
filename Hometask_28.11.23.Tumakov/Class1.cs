3e101853683449ab71e503e23b5ac329e5fba76b
Timur <zamilov05@mail.ru>
2023-12-02 14:40:53 +0300

    первый тестовый коммит

---
+++ Hometask_28.11.23.Tumakov/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask_28._11._23.Tumakov
+{
+    internal class Class1
+    {
+    }
+}
